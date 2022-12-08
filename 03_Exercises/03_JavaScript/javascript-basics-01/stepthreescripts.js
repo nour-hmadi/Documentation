e34b728103607b67977c5de06c78787b70985280
nour <nour.hmadi.a@gmail.com>
2022-12-08 10:46:20 +0200

    Completed stepthree-javascript

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/stepthreescripts.js
@@ -0,0 +1,16 @@
+
+
+
+
+    var myName = prompt("Enter your name ");
+
+    alert("Hello " + myName);
+
+    /*document.write("You have entered : " + myName);*/
+ 
+
+ /*
+var myName=document.getElementById("myName");
+
+
+    alert("Hello " + myName);
