cb3c9723348802c4bac5997a5407daaf67b22952
nour <nour.hmadi.a@gmail.com>
2022-12-08 16:45:23 +0200

    Completed stepfour-javascript

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/stepfourscripts.js
@@ -0,0 +1,21 @@
+
+var myName
+myName=document.getElementById("name");
+myName = prompt("Enter your name ");
+
+
+
+var mysurname;
+mysurname=document.getElementById("surname");
+mysurname = prompt("Enter your surname ");
+
+
+
+
+var mycity
+mycity=document.getElementById("city");
+mycity = prompt("Enter your city ");
+
+
+
+alert('name: '+myName+ '\n'+'surname: '+mysurname+'\n'+'City: '+mycity);
