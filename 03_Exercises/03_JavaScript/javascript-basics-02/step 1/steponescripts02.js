4deeeb03b5a85d40ba8daef4e9d6facc9d482f31
nour <nour.hmadi.a@gmail.com>
2022-12-09 10:30:13 +0200

    lastupdate stepone-javascript02

--- 03_Exercises/03_JavaScript/javascript-basics-02/step 1/steponescripts02.js
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 1/steponescripts02.js
@@ -1,3 +1,13 @@
-function nicething(){
-   document.getElementById("image1").src = "images/image1_2.jpg";
-}
+// function nicething(){
+//     document.querySelector("#image1").src = "images/image1_2.jpg";
+//  }
+//with this method i go to html and add onmouseover="nicething()"
+//it is really a  nice thing :) :D
+
+
+ var x=document.querySelector('#image1');
+ x.addEventListener('mouseover',function(){
+     x.src="images/image1_2.jpg";
+ })
+
+
