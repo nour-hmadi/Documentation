bfed0e44ae5eb99481f01914d8a11f2575fc617f
nour <nour.hmadi.a@gmail.com>
2022-12-09 13:11:49 +0200

    Completed steptwo-javascript02

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 2/steptwoscripts02.js
@@ -0,0 +1,40 @@
+// we can use document.getelementbyId('image1')
+//or we can use document.queryselector('#)(to spot an identity)
+// document.queryselector('.)(to spot a class)
+//and more to come :p
+
+
+
+
+
+
+
+
+
+
+
+
+var x=document.querySelector('#name');
+x.addEventListener("blur",nicething2);
+
+function nicething2(){
+alert("thank you for participating");
+}
+
+
+
+
+// function myBlurFunction() {
+    // document.getElementById("myInput").style.backgroundColor = "";
+//   }
+
+
+
+
+
+
+
+
+
+
+
