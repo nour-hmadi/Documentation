850d454c303c8a9787373885356b2361900e36cf
nour <nour.hmadi.a@gmail.com>
2022-12-09 15:34:06 +0200

    last update stepfour-javascripts02

--- 03_Exercises/03_JavaScript/javascript-basics-02/step 4/stepfourscripts02.js
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 4/stepfourscripts02.js
@@ -5,15 +5,29 @@
 
 
 
-function afunction(){
+// function afunction(){
+//     var nameX= document.getElementById("name");
+//     var surnameX= document.getElementById("city");
+//     var cityX= document.getElementById("surname");
+
+//       if ((nameX.value !="")  || (surnameX.value !="") || (cityX.value !="")) {
+//           nameX.value = "";
+//           surnameX.value = "";
+//           cityX.value = "";
+        
+
+// }
+
+
+//
+function reset1(){
     var nameX= document.getElementById("name");
     var surnameX= document.getElementById("city");
     var cityX= document.getElementById("surname");
-
-      if ((nameX.value !="")  || (surnameX.value !="") || (cityX.value !="")) {
-          nameX.value = "";
-          surnameX.value = "";
-          cityX.value = "";
-        }
+if(confirm("do you wanna reset?")){
+    nameX.value = "";
+    surnameX.value = "";
+    cityX.value = "";
+}
 
 }
