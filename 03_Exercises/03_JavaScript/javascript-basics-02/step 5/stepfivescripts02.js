e0f612ed02e9e48a74329b7e82504a4701a878c7
nour <nour.hmadi.a@gmail.com>
2022-12-09 16:27:58 +0200

    Completed step5-step6-javascripts02

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 5/stepfivescripts02.js
@@ -0,0 +1,22 @@
+function pic1(x){
+if(x==1)
+{
+    document.getElementById("image1").src= "images/image1_2.jpg";
+}
+else if(x==2)
+{
+    document.getElementById("image2").src="images/image2_2.jpg";
+}
+ else if(x==3)
+{
+    document.getElementById("image3").src="images/image3_2.jpg";
+}
+else if(x==4)
+{
+    document.getElementById("image4").src="images/image4_2.jpg";
+}
+else if(x==5)
+{
+    document.getElementById("image5").src="images/image5_2.jpg";
+}
+}
---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 6/stepsix02.js
@@ -0,0 +1,44 @@
+function pic1(x){
+    if(x==1)
+    {
+        document.getElementById("image1").src= "images/image1_2.jpg";
+    }
+    else if(x==2)
+    {
+        document.getElementById("image2").src="images/image2_2.jpg";
+    }
+     else if(x==3)
+    {
+        document.getElementById("image3").src="images/image3_2.jpg";
+    }
+    else if(x==4)
+    {
+        document.getElementById("image4").src="images/image4_2.jpg";
+    }
+    else if(x==5)
+    {
+        document.getElementById("image5").src="images/image5_2.jpg";
+    }
+    }
+    function pic2(x){
+        if(x==1)
+        {
+            document.getElementById("image1").src= "images/image1.jpg";
+        }
+        else if(x==2)
+        {
+            document.getElementById("image2").src="images/image2.jpg";
+        }
+         else if(x==3)
+        {
+            document.getElementById("image3").src="images/image3.jpg";
+        }
+        else if(x==4)
+        {
+            document.getElementById("image4").src="images/image4.jpg";
+        }
+        else if(x==5)
+        {
+            document.getElementById("image5").src="images/image5.jpg";
+        }
+        }
