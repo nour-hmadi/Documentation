1ddec4dfd8ecc6f91cc26287b6c16bc568aeec54
nour <nour.hmadi.a@gmail.com>
2022-12-13 00:08:56 +0200

    Creating Table

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -15,4 +15,27 @@
 
 7.UPDATE students SET Points = "150" WHERE name = "Alex"
 
-8.
+8. 
+9. Done
+
+
+II.  CREATE TABLE graduates (
+	ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	Name TEXT NOT NULL UNIQUE,
+	Age INTEGER,
+	Gender TEXT,
+	Points INTEGER,
+	Graduation date)
+
+10. INSERT INTO graduates(ID, name, Age, Gender, Points) 
+    SELECT ID, name, Age, Gender, Points FROM students 
+    WHERE name = "Layal";
+
+11. UPDATE graduates 
+    SET Graduation = "2018-09-08"   
+    WHERE name = "Layal";
+
+12. DELETE FROM students WHERE name = "Layal";
+
+
+III.
